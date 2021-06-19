ec9c3132b1911bcb0580966d295b33b6e51e418b
zl <zl03jsj@163.com>
2021-06-19 16:15:45 +0800

    es_newindx, ex_rm_old_idx

--- es_rollover_newidx.sh
+++ es_rollover_newidx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -it --rm --net=host -e CONDITIONS='{"max_age": "60s"}' jaegertracing/jaeger-es-rollover:latest rollover http://localhost:9200
+docker run -it --rm --net=host \
+    -e CONDITIONS='{"max_age": "60s"}' \
+    jaegertracing/jaeger-es-rollover:latest \
+    rollover \
+    http://localhost:9200
--- es_rollover_rm_oldidx.sh
+++ es_rollover_rm_oldidx.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-docker run -it --rm --net=host -e ROLLOVER=true jaegertracing/jaeger-es-index-cleaner:latest 0 http://localhost:9200
+# remove old indices from read aliases.
+# It means that old data will not be available for search.
+# This imitates the behavior of --es.max-span-age flag used in the default index-per-day deployment.
+# This step could be optional and 
+# old indices could be simply removed by index cleaner in the next step.
+docker run -it --rm --net=host -e UNIT=seconds -e \
+    UNIT_COUNT=2 \
+    jaegertracing/jaeger-es-rollover:latest lookback \
+    http://localhost:9200
+
+# Remove indices older than 2*60 seconds
+
+# The historical data can be removed with the jaeger-es-index-cleaner 
+# that is also used for daily indices.
+
+docker run -it --rm --net=host \
+    -e ROLLOVER=true \
+    jaegertracing/jaeger-es-index-cleaner:latest 2 \
+    http://localhost:9200
