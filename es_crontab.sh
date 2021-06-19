bddfdf9b71d439c9e1e5be0482db6d4249f335db
zl <zl03jsj@163.com>
2021-06-19 18:20:24 +0800

    rollover crontab shell

--- es_crontab.sh
+++ es_crontab.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
+./es_rollover_newidx.sh
+./es_rollover_rm_oldidx.sh
 
-main() {
-    curl -H'Content-Type:application/json' -d'{
-    "query": { "range": { "startTimeMillis": {"lt":"now-2h", "format":"epoch_millis"} } }
-    }' -XPOST "$ES_URL_AND_PORT/*-*/_delete_by_query?pretty" 
-}
-
-main "$@"
