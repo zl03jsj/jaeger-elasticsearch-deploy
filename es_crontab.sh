26d2c0e55d84b0d9d83ed56198cfb8c4970a4185
zl <zl03jsj@163.com>
2021-06-22 17:21:33 +0800

    branch for running on local machine

--- es_crontab.sh
+++ es_crontab.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+time=$(date "+%Y-%m-%d %H:%M:%S")
+echo rollover_newidx at time=$time >> cron.log
 ./es_rollover_newidx.sh
 
+echo rollover rm old index at time=$time >> cron.log
 ./es_rollover_rm_oldidx.sh
+
+echo >> cron.log
 
