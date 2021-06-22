b36df992fbd21151a9048d2ba61eca95cd4f4de9
zl <zl03jsj@163.com>
2021-06-22 13:33:04 +0800

    optmize

--- es_rollover_init.sh
+++ es_rollover_init.sh
@@ -2,11 +2,16 @@
 
 source ./rollover_configurations
 
-docker run --privileged --name esrollover -it --rm --net=host \
-    jaegertracing/jaeger-es-rollover:1.23 \
-    init $elasticsearch_url
+#docker run --privileged --name esrollover -it --rm --net=host \
+#    jaegertracing/jaeger-es-rollover:1.23 \
+#    init $elasticsearch_url
+
+esmapgenFile=$GOBIN/esmapping-generator
+
+if [ ! -f "$esmapgenFile" ]; then
+  go get github.com/jaegertracing/jaeger/cmd/esmapping-generator@v1.23.0
+fi
 
 # 需要编译esmapping-generator,所以没办法直接使用
-#python3 ./es_rollover.py init ${elasticsearch_url}
-
+python3 ./es_rollover.py init ${elasticsearch_url}
 
